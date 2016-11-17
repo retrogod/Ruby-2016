9557161c478937aed18d4d6f2442c9c15123900a
Matthias Merlo <s2mamerl@uni-trier.de>
2016-11-17 12:36:02 +0100

    Updated 1.3 & 1.4

---
+++ Abgaben/ruby_aufgabe1_4.rb
@@ -0,0 +1,10 @@
+def string_to_hash(string)
+  words = string.split(" ")
+  array_length = words.length
+  hash = Hash.new
+  i = 0
+  for i < array_length
+    hash += words[i]
+  end
+  return hash
+end
