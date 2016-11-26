51bba87e54805e179a192c869cab04a00b1bffad
Matthias Merlo <s2mamerl@uni-trier.de>
2016-11-26 12:41:06 +0100

    U2

---
+++ Abgaben/Uebung 2/aufgabe2.rb
@@ -0,0 +1,18 @@
+#!/bin/ruby
+
+class Dokument
+  def initialize(title, text)
+    @title = title
+	@text = text
+  end
+  
+  def schreibweise(input)
+    stext = @text.split
+	stext.
+  end
+  
+  def kontext
+  
+  end
+
+end
