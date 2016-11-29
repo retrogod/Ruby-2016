8adbe11df50fffff4ea29b45eb3b83ca5d5c1456
Matthias Merlo <s2mamerl@uni-trier.de>
2016-11-29 21:50:28 +0100

    1.1

--- Abgaben/Uebung 2/aufgabe2.rb
+++ Abgaben/Uebung 2/aufgabe2.rb
@@ -1,18 +1,29 @@
-#!/bin/ruby
-
 class Dokument
-  def initialize(title, text)
-    @title = title
-	@text = text
+  
+  def initialize(inhalt)
+    #@titel = titel
+	#@erscheinungsjahr = erscheinungsjahr
+	#@author = author
+	$inhalt = inhalt
   end
   
+  #Anfang Aufgabe 1
   def schreibweise(input)
-    stext = @text.split
-	stext.
+    res = []
+	$inhalt.scan(input) do |c| res << [c, $~.offset(0)[0]]
+	res.inspect
+	return res
+  end
   end
   
-  def kontext
   
-  end
+  
+  #Anfang Aufgabe 2
+  #def kontext
+  
+  #end
 
 end
+
+testdoc = Dokument.new("Die Der Das Die Der Das Die Der Das Die Der Das Die Der Das Die Der Das Die Der Das")
+testdoc.schreibweise("Die")
